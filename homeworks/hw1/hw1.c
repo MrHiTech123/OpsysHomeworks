54432b3ec32a70e02c67ffe33e98b58aae2c574f
MrHiTech123 <78568080+MrHiTech123@users.noreply.github.com>
2026-01-24 13:54:26 -0500

    Hw1

---
+++ homeworks/hw1/hw1.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+int fileLength(FILE* file) {
+	fseek(file, 0, SEEK_END);
+	int toReturn = ftell(file);
+	rewind(file);
+	return toReturn;
+}
+
+char* callocFileContentsBuffer(const char* fileName) {
+	FILE* file = fopen(fileName, "r");
+	int length = fileLength(file);
+	printf("%d\n", length);
+	
+	
+	char* toReturn = calloc(length + 1, sizeof(char));
+	fread(toReturn, sizeof(char), length, file);
+	fclose(file);
+	*(toReturn + length) = '\0';
+	
+	return toReturn;
+	
+}
+
+
+
+int main(int argc, char const *argv[])
+{
+	if (argc == 1) {
+		printf("Usage hw1 tokenfile [maxTokenLength]");
+	}
+	if (argc == 2) {
+		char const ** nextArgv = calloc(3, sizeof(char*));
+		*(nextArgv + 1) = *(argv);
+		*(nextArgv + 2) = "100";
+		
+		int toReturn = main(
+			2,
+			nextArgv
+		);
+		
+		free(nextArgv);
+		return toReturn;
+	}
+	
+	char* fileContents = callocFileContentsBuffer(*(argv + 1));
+	
+	printf("%s\n", argv[1]);
+	printf("%s\n", fileContents);
+	
+	free(fileContents);
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
+
