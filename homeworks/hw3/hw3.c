7875bf599a56aac41c1e056eec25c69e482d6aaf
MrHiTech123 <78568080+MrHiTech123@users.noreply.github.com>
2026-03-29 21:17:02 -0400

    Sorting works

--- homeworks/hw3/hw3.c
+++ homeworks/hw3/hw3.c
@@ -8,7 +8,7 @@
 
 #define createArray(type, len) (nameOfArrayType(type)){.values = calloc((len), sizeof(type)), .length = (len)}
 #define freeArray(array) free(array.values);
-#define copyArray(type, src, start, end) ({int len = ((end) - (start)); nameOfArrayType(type) toReturn = createArray(type, len); memcpy(toReturn.values, src.values + start, len * sizeof(type)); toReturn;})
+#define copyArray(type, src, start, end) ({int len = ((end) - (start)); nameOfArrayType(type) toReturn = createArray(type, len); memcpy(toReturn.values, src.values + (start), len * sizeof(type)); toReturn;})
 #define copyArrayStart(type, src, start) copyArray(type, src, start, src.length)
 #define copyArrayEnd(type, src, end) copyArray(type, src, 0, end)
 #define copyArrayFull(type, src) copyArray(type, src, 0, src.length)
@@ -39,8 +39,10 @@ void freeNestedIntArray(Array_Array_int toFree) {
 Array_Array_int divide_createArray(Array_int arr) {
 	Array_Array_int toReturn = createArray(Array_int, 2);
 	
-	at(toReturn, 0) = copyArrayEnd(int, arr, arr.length / 2 + arr.length % 2);
-	at(toReturn, 1) = copyArrayStart(int, arr, arr.length / 2);
+	int divisionPoint = arr.length / 2 + arr.length % 2;
+	
+	at(toReturn, 0) = copyArrayEnd(int, arr, divisionPoint);
+	at(toReturn, 1) = copyArrayStart(int, arr, divisionPoint);
 	
 	return toReturn;
 }
@@ -80,9 +82,10 @@ Array_int merge_createArray(const Array_int first, const Array_int second) {
 	
 }
 
+#define mergeSort(arr) ({void* ptr = voidVoidMergeSort(&(arr)); Array_int toReturn = *(Array_int*)ptr; free(ptr); toReturn;})
 
 
-void* mergeSort(void* arrayVoid) {
+void* voidVoidMergeSort(void* arrayVoid) {
 	Array_int array = *(Array_int*)arrayVoid;
 	Array_int* toReturn = calloc(1, sizeof(Array_int));
 	
@@ -95,8 +98,10 @@ void* mergeSort(void* arrayVoid) {
 	Array_Array_int halves = divide_createArray(array);
 	
 	Array_Array_int sortedHalves = createArray(Array_int, 2);
-	at(sortedHalves, 0) = *(Array_int*)mergeSort(&at(halves, 0));
-	at(sortedHalves, 1) = *(Array_int*)mergeSort(&at(halves, 1));
+	
+	
+	at(sortedHalves, 0) = mergeSort(at(halves, 0));
+	at(sortedHalves, 1) = mergeSort(at(halves, 1));
 	
 	Array_int sorted = merge_createArray(at(sortedHalves, 0), at(sortedHalves, 1));
 	
@@ -107,6 +112,7 @@ void* mergeSort(void* arrayVoid) {
 	return toReturn;
 }
 
+
 int hw3() {
 	Array_int originalRealArray = {.values = numbers, .length = n};
 	Array_int arrayToSort = copyArrayFull(int, originalRealArray);
@@ -115,13 +121,18 @@ int hw3() {
 		printIntArray(arrayToSort);
 	#endif
 	
-	Array_int sorted = *(Array_int*)mergeSort(&arrayToSort);
+	Array_int sorted = mergeSort(arrayToSort);
+	
+	freeArray(arrayToSort);
+	freeArray(originalRealArray);
+	
 	
 	#ifdef DEBUG
 		printIntArray(sorted);
 	#endif
 	
-	
+	numbers = sorted.values;
+	n = sorted.length;
 	
 	return EXIT_SUCCESS;
 }
