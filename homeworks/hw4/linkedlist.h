b15b1a1812f12601d3dc409922378afd67e93d2f
MrHiTech123 <78568080+MrHiTech123@users.noreply.github.com>
2026-04-24 00:32:56 -0400

    Working

--- homeworks/hw4/linkedlist.h
+++ homeworks/hw4/linkedlist.h
@@ -6,13 +6,13 @@ typedef char* str;
 #define linkedlistNodeTypeName(type) LinkedListNode_##type
 #define linkedlistTypeName(type) LinkedList_##type
 
-#define UNIQUE_NAME l ## __LINE__
-#define LinkedList__foreach(type, varname, list) if (list) {type varname = list->value; for (linkedlistTypeName(type) UNIQUE_NAME = list; UNIQUE_NAME; ({UNIQUE_NAME = UNIQUE_NAME->next; varname = (UNIQUE_NAME)? UNIQUE_NAME->value : varname;}))}
+#define UNIQUE(name) name ## __LINE__
+#define LinkedList__foreach(type, varname, list) type varname = list->value; for (linkedlistTypeName(type) UNIQUE(currentList) = list; UNIQUE(currentList); ({UNIQUE(currentList) = UNIQUE(currentList)->next; varname = (UNIQUE(currentList))? UNIQUE(currentList)->value : varname;}))
 #define LinkedList_create NULL
 #define LinkedList_createNodePtr(type) calloc(1, sizeof(linkedlistNodeTypeName(type)))
-#define LinkedList_append(type, list, v) if (list) {linkedlistTypeName(type) current##__LINE__ = list; while (current##__LINE__->next) {current##__LINE__ = current##__LINE__->next;}; linkedlistTypeName(type) next = LinkedList_createNodePtr(type); next->value = v; current##__LINE__->next = next;} else {list = LinkedList_createNodePtr(type); list->value = v;}
-#define LinkedList_free(type, list) {linkedlistTypeName(type) UNIQUE_NAME = list; while (UNIQUE_NAME) {linkedlistTypeName(type) next = UNIQUE_NAME->next; free(UNIQUE_NAME); UNIQUE_NAME = next;}}
-
+#define LinkedList_append(type, list, v) if (list) {linkedlistTypeName(type) UNIQUE(current) = list; while (UNIQUE(current)->next) {UNIQUE(current) = UNIQUE(current)->next;}; linkedlistTypeName(type) next = LinkedList_createNodePtr(type); next->value = v; UNIQUE(current)->next = next;} else {list = LinkedList_createNodePtr(type); list->value = v;}
+#define LinkedList_free(type, list) {linkedlistTypeName(type) UNIQUE(current) = list; while (UNIQUE(current)) {linkedlistTypeName(type) next = UNIQUE(current)->next; free(UNIQUE(current)); UNIQUE(current) = next;}}
+#define LinkedList_length(type, list) {linkedlistTypeName(type) UNIQUE(current) = list; int toReturn = 0; }
 
 #define DECLARE_LINKEDLIST_TYPE(type) typedef struct linkedlistNodeTypeName(type) {struct linkedlistNodeTypeName(type)* next; type value;} linkedlistNodeTypeName(type); typedef linkedlistNodeTypeName(type)* linkedlistTypeName(type)
 
