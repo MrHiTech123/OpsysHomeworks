7ae1beebc54d8744fa23ea7826ae0d5fb972313c
MrHiTech123 <78568080+MrHiTech123@users.noreply.github.com>
2026-04-24 17:36:06 -0400

    Progress

--- homeworks/hw4/linkedlist.h
+++ homeworks/hw4/linkedlist.h
@@ -1,19 +1,21 @@
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef char* str;
 
-
-#define linkedlistNodeTypeName(type) LinkedListNode_##type
-#define linkedlistTypeName(type) LinkedList_##type
+#define LINKEDLIST_NODETYPE_NAME(type) LinkedListNode_##type
+#define LINKEDLIST_TYPE_NAME(type) LinkedList_##type
 
 #define MACRO_VAR(name) macroVar_ ## name ## __LINE__
-#define LinkedList__foreach(type, varname, list) type varname; if (list.first) {varname = list.first->value;}; for (linkedlistNodeTypeName(type)* MACRO_VAR(currentList) = list.first; MACRO_VAR(currentList); ({MACRO_VAR(currentList) = MACRO_VAR(currentList)->next; varname = (MACRO_VAR(currentList))? MACRO_VAR(currentList)->value : varname;}))
+#define LinkedList__foreach(type, varname, list) type varname; if (list.first) {varname = list.first->value;}; for (LINKEDLIST_NODETYPE_NAME(type)* MACRO_VAR(currentList) = list.first; MACRO_VAR(currentList); ({MACRO_VAR(currentList) = MACRO_VAR(currentList)->next; varname = (MACRO_VAR(currentList))? MACRO_VAR(currentList)->value : varname;}))
 #define LinkedList__create {.first = NULL, .length = 0}
-#define LinkedList__createNodePtr(type, v) ({linkedlistNodeTypeName(type)* MACRO_VAR(toReturn) = calloc(1, sizeof(linkedlistNodeTypeName(type))); MACRO_VAR(toReturn)->value = v; MACRO_VAR(toReturn);})
-#define LinkedList__append(type, list, v) {++list.length; linkedlistNodeTypeName(type)* MACRO_VAR(current) = list.first; while (MACRO_VAR(current) && MACRO_VAR(current)->next) {MACRO_VAR(current) = MACRO_VAR(current)->next;}; linkedlistNodeTypeName(type)* MACRO_VAR(next) = LinkedList__createNodePtr(type, v); if (list.first) {MACRO_VAR(current)->next = MACRO_VAR(next);} else {list.first = MACRO_VAR(next);};}
-#define LinkedList__free(type, list) {linkedlistNodeTypeName(type)* MACRO_VAR(current) = list.first; while (MACRO_VAR(current)) {linkedlistNodeTypeName(type)* MACRO_VAR(next) = MACRO_VAR(current)->next; free(MACRO_VAR(current)); MACRO_VAR(current) = MACRO_VAR(next);}}
+#define LinkedList__createNodePtr(type, v) ({LINKEDLIST_NODETYPE_NAME(type)* MACRO_VAR(toReturn) = calloc(1, sizeof(LINKEDLIST_NODETYPE_NAME(type))); MACRO_VAR(toReturn)->value = v; MACRO_VAR(toReturn);})
+#define LinkedList__append(type, list, v) {++list.length; LINKEDLIST_NODETYPE_NAME(type)* MACRO_VAR(current) = list.first; while (MACRO_VAR(current) && MACRO_VAR(current)->next) {MACRO_VAR(current) = MACRO_VAR(current)->next;}; LINKEDLIST_NODETYPE_NAME(type)* MACRO_VAR(next) = LinkedList__createNodePtr(type, v); if (list.first) {MACRO_VAR(current)->next = MACRO_VAR(next);} else {list.first = MACRO_VAR(next);};}
+#define __LINKED_LIST_FREE(type, list, freeFunction) {LINKEDLIST_NODETYPE_NAME(type)* MACRO_VAR(current) = list.first; while (MACRO_VAR(current)) {LINKEDLIST_NODETYPE_NAME(type)* MACRO_VAR(next) = MACRO_VAR(current)->next; freeFunction(MACRO_VAR(current)); MACRO_VAR(current) = MACRO_VAR(next);}}
+#define LinkedList__free(type, list) __LINKED_LIST_FREE , free
+#define LinkedList__free(type, list)
 
-#define DECLARE_LINKEDLIST_TYPE(type) typedef struct linkedlistNodeTypeName(type) {struct linkedlistNodeTypeName(type)* next; type value;} linkedlistNodeTypeName(type); typedef struct {linkedlistNodeTypeName(type)* first; int length;} linkedlistTypeName(type)
+#define DECLARE_LINKEDLIST_TYPE(type) typedef struct LINKEDLIST_NODETYPE_NAME(type) {struct LINKEDLIST_NODETYPE_NAME(type)* next; type value;} LINKEDLIST_NODETYPE_NAME(type); typedef struct {LINKEDLIST_NODETYPE_NAME(type)* first; int length;} LINKEDLIST_TYPE_NAME(type)
 
 
 DECLARE_LINKEDLIST_TYPE(int);
