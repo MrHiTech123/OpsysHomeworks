b291a4cc52252b592841bd79ad7c67d924763ff4
MrHiTech123 <78568080+MrHiTech123@users.noreply.github.com>
2026-04-24 00:21:10 -0400

    Foreach works with empty

--- homeworks/hw4/linkedlist.h
+++ homeworks/hw4/linkedlist.h
@@ -7,7 +7,7 @@ typedef char* str;
 #define linkedlistTypeName(type) LinkedList_##type
 
 #define UNIQUE_NAME l ## __LINE__
-#define LinkedList__foreach(type, varname, list) type varname = list->value; for (linkedlistTypeName(type) UNIQUE_NAME = list; UNIQUE_NAME; ({UNIQUE_NAME = UNIQUE_NAME->next; varname = (UNIQUE_NAME)? UNIQUE_NAME->value : varname;}))
+#define LinkedList__foreach(type, varname, list) if (list) {type varname = list->value; for (linkedlistTypeName(type) UNIQUE_NAME = list; UNIQUE_NAME; ({UNIQUE_NAME = UNIQUE_NAME->next; varname = (UNIQUE_NAME)? UNIQUE_NAME->value : varname;}))}
 #define LinkedList_create NULL
 #define LinkedList_createNodePtr(type) calloc(1, sizeof(linkedlistNodeTypeName(type)))
 #define LinkedList_append(type, list, v) if (list) {linkedlistTypeName(type) current##__LINE__ = list; while (current##__LINE__->next) {current##__LINE__ = current##__LINE__->next;}; linkedlistTypeName(type) next = LinkedList_createNodePtr(type); next->value = v; current##__LINE__->next = next;} else {list = LinkedList_createNodePtr(type); list->value = v;}
