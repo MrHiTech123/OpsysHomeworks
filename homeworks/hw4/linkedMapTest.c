dbc03ae98084c66bc1d171db170f30dd8178abc4
MrHiTech123 <78568080+MrHiTech123@users.noreply.github.com>
2026-04-28 01:24:21 -0400

    Linked map test works

--- homeworks/hw4/linkedMapTest.c
+++ homeworks/hw4/linkedMapTest.c
@@ -2,7 +2,23 @@
 
 int main(int argc, char const *argv[])
 {
-	Map_int_str map = LinkedMap__create;
+	LinkedMap_int_str map = LinkedMap__create;
+	
+	LinkedMapDataStruct_int_str x = {.key = 1, .value = "Hi"};
+	
+	// LinkedList__append(LINKEDMAP_DATA_STRUCT_NAME(int, str), map, {.key = 1; .value = "Hi"});
+	
+	LinkedMap__insert(int, str, map, 1, "HI");
+	// {++map.length; LinkedListNode_LinkedMapDataStruct_int_str* macroVar_current__LINE__ = map.first; while (macroVar_current__LINE__ && macroVar_current__LINE__->next) {macroVar_current__LINE__ = macroVar_current__LINE__->next;}; LinkedListNode_LinkedMapDataStruct_int_str* macroVar_next__LINE__ = ({LinkedListNode_LinkedMapDataStruct_int_str* macroVar_toReturn__LINE__ = calloc(1, sizeof(LinkedListNode_LinkedMapDataStruct_int_str)); macroVar_toReturn__LINE__->value = (LinkedMapDataStruct_int_str){.key = 1, .value = "HI"}; macroVar_toReturn__LINE__;}); if (map.first) {macroVar_current__LINE__->next = macroVar_next__LINE__;} else {map.first = macroVar_next__LINE__;};}
+	
+	LinkedMap__insert(int, str, map, 2, "Bye");
+	
+	printf("%s\n", LinkedMap__find(int, str, map, 2, equals));
+	
+	// ({str toReturn; LinkedMapDataStruct_int_str macroVar_current__LINE__; if (map.first) {macroVar_current__LINE__ = map.first->value;}; for (LinkedListNode_LinkedMapDataStruct_int_str* macroVar_currentList__LINE__ = map.first; macroVar_currentList__LINE__; ({macroVar_currentList__LINE__ = macroVar_currentList__LINE__->next; macroVar_current__LINE__ = (macroVar_currentList__LINE__)? macroVar_currentList__LINE__->value : macroVar_current__LINE__;})) {if (macroVar_current__LINE__.key == 2) {toReturn = macroVar_current__LINE__.value;}}; toReturn});
+
+	
+	
 	
 	
 	
--- homeworks/hw4/linkedmap.h
+++ homeworks/hw4/linkedmap.h
@@ -7,14 +7,20 @@
 #define LINKEDMAP_DATA_STRUCT_NAME(keyType, valueType) LinkedMapDataStruct_ ## keyType ## _ ## valueType
 #define LINKEDMAP_DATA_STRUCT_LINKED_LIST_TYPE_NAME(keyType, valueType) LINKEDLIST_TYPE_NAME(LinkedMapDataStruct_ ## keyType ## _ ## valueType)
 #define LINKEDMAP_NODE_NAME(keyType, valueType) LINKEDLIST_NODETYPE_NAME(LinkedMapDataStruct_ ## keyType ## _ ## valueType)
-#define LINKEDMAP_TYPE_NAME(keyType, valueType) Map_ ## keyType ## _ ## valueType
+#define LINKEDMAP_TYPE_NAME(keyType, valueType) LinkedMap_ ## keyType ## _ ## valueType
 #define DECLARE_LINKEDMAP_TYPE(keyType, valueType) typedef struct {keyType key; valueType value;} LINKEDMAP_DATA_STRUCT_NAME(keyType, valueType); DECLARE_LINKEDLIST_TYPE(LINKEDMAP_DATA_STRUCT_NAME(keyType, valueType)); typedef LINKEDMAP_DATA_STRUCT_LINKED_LIST_TYPE_NAME(keyType, valueType) LINKEDMAP_TYPE_NAME(keyType, valueType)
 // typedef LinkedList__ ## LINKEDMAP_DATA_STRUCT_NAME(keyType, valueType) LINKEDMAP_TYPE_NAME(keyType, valueType)
 
-#define MAP_FIND_ELEMENT(key, value)
 
 DECLARE_LINKEDMAP_TYPE(int, str);
+DECLARE_LINKEDMAP_TYPE(str, LinkedList_str);
 
 // LinkedList_LinkedMapDataStruct_int_str
 #define LinkedMap__create LinkedList__create
+
+#define equals(x, y) x == y
+
+#define LinkedMap__insert(keyType, valueType, map, k, v) {LINKEDMAP_DATA_STRUCT_NAME(keyType, valueType) MACRO_VAR(toAppend) = {.key = k, .value = v}; LinkedList__append(LINKEDMAP_DATA_STRUCT_NAME(keyType, valueType), map, MACRO_VAR(toAppend))}
+#define LinkedMap__find(keyType, valueType, map, k, equalityFunction) ({valueType toReturn; LinkedList__foreach(LINKEDMAP_DATA_STRUCT_NAME(keyType, valueType), MACRO_VAR(current), map) {if (equalityFunction(MACRO_VAR(current).key, k)) {toReturn = MACRO_VAR(current).value; break;}}; toReturn;})
+
 
